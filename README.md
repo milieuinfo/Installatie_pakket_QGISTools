dc4d613f22fb07fb940566b3b9399124e01f0ab8
warrieka <kaywarrie@gmail.com>
2016-01-18 19:17:13 +0100

    create a plugin repo

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Install package for the QGIS-tools for LNE
 ====
 
+*I have create 2 ways of deploy QGIS-plugins without having go through the official channels:*
+
+A Windows install package
+----
+
 NSIS-scripts that will install the different QGIS-tools developed for LNE.
 
 There are 2 versions:
@@ -10,4 +15,19 @@ There are 2 versions:
 
 The inputs folder is left empty deliberately and should be filled with the latest compiled version of the different tools.  
 
-In the build-folder will contain the installer after you run the scripts. 
+In the build-folder will contain the installer after you run the scripts.
+
+
+Repository generator
+----
+
+If you deploy the contents of plugin_server on a webserver and add {url_of_server}/plugins/plugins.xml to the plugins repository list the plugins will be available for installation just like other plugins. 
+
+createPluginServerContent.py {url_of_server}    
+    
+For example, if running locally on localhost:
+
+    createPluginServerContent.py http://localhost:8000/
+    
+Then just add http://localhost:8000/plugins/plugins.xml to the list of repositories:
+Plugins > Manage and install Plugins > Settings > Plugin repositories > Add
---
+++ createPluginRepoContent.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+import sys, os, ConfigParser, glob, datetime
+import zipfile
+
+defaultPluginPage = 'http://localhost:8000'
+currentDir = os.path.dirname(os.path.realpath(__file__))
+
+def readIni(iniFile):
+    meta ={}
+    
+    config = ConfigParser.ConfigParser()
+    config.readfp( open(iniFile) )
+
+    meta['name'] = config.get('general', 'name')
+    meta['qgisMinimumVersion'] = config.get('general', 'qgisMinimumVersion')
+    meta['description'] = config.get('general', 'description')
+    meta['about'] = config.get('general', 'about')
+    meta['version'] = config.get('general', 'version')
+    
+    meta['author'] = config.get('general', 'author')
+    meta['email'] = config.get('general', 'email')
+    
+    meta['repository'] = config.get('general', 'repository')
+    meta['homepage'] = config.get('general', 'homepage')
+    meta['tracker'] = config.get('general', 'tracker')
+    
+    meta['experimental'] = config.get('general', 'experimental')
+    meta['tags'] = config.get('general', 'tags')
+    return meta
+
+def createPluginXML(metaParams, DLsite):
+    head= """<?xml version = '1.0' encoding = 'UTF-8'?>
+    <?xml-stylesheet type="text/xsl" href="/static/style/plugins.xsl" ?>
+    <plugins>"""
+    foot = "</plugins>"
+
+    xml = head 
+    count = 0
+    
+    for metaParam in metaParams:
+        count += 1
+        now = datetime.datetime.now().isoformat()
+        DLpath = os.path.join(DLsite, 'download', metaParam['zip'] )
+
+        xml += """<pyqgis_plugin name="{name}" version="{version}" plugin_id="{c}">
+        <description><![CDATA[ {description} ]]></description>
+        <about>{about}</about>
+        <author_name>{author}</author_name>
+        <version>{version}</version>
+        <qgis_minimum_version>{qgisMinimumVersion}</qgis_minimum_version>
+        <qgis_maximum_version>2.99</qgis_maximum_version>
+        <file_name>{zip}</file_name>
+        <download_url>{site}</download_url>
+        <create_date>{now}</create_date>
+        <update_date>{now}</update_date>
+        <experimental>{experimental}</experimental>
+        <deprecated>False</deprecated>
+        <homepage>{homepage}</homepage>
+        <tracker>{tracker}</tracker>
+        <repository>{repository}</repository>
+        <tags>{tags}</tags>
+    </pyqgis_plugin>""".format(site=DLpath, now=now, c=count, **metaParam )
+    
+    xml += foot
+    
+    return xml
+
+def zipPath(root, target):
+    if os.path.exists( target ):
+       os.remove(target)
+    zipf = zipfile.ZipFile( target , 'w')
+    
+    files = []
+    for path, subdirs, filenames in os.walk(root):
+        for filename in filenames:
+            files.append( os.path.join(path, filename) )
+            
+    for zfile in files: 
+        sbase = os.path.dirname(root)
+        arcName = zfile.replace( sbase ,"")
+        zipf.write( zfile , arcName) 
+
+    zipf.close() 
+
+def main( pluginPage, cwd):
+   metaParams = []
+   pluginDir = cwd + "/inputs/"
+   
+   pluginMetadata = glob.glob( pluginDir + "*/metadata.txt")
+   
+   for ini in pluginMetadata:
+       meta = readIni(ini)
+       
+       dir2zip = os.path.dirname(ini) 
+       zipName = meta['name'].replace(' ', '_') + '.zip'
+       zipTarget = os.path.join( cwd, 'plugin_server/download', zipName )
+       zipPath(dir2zip, zipTarget)
+       
+       meta['zip'] = zipName
+       metaParams.append(meta)
+   
+   with open( os.path.join(cwd, 'plugin_server/plugins/plugins.xml'), 'w') as plugins:
+        plugins.write( createPluginXML(metaParams, pluginPage) )  
+        
+   
+if __name__ == '__main__': 
+    if len( sys.argv ) == 3:
+        main(sys.argv[1],sys.argv[2])
+    elif len( sys.argv ) == 2:
+        main(sys.argv[1], currentDir)
+    else:
+        main( defaultPluginPage, currentDir)
+        
+        
